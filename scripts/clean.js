ecbaeb7660f029a80c535c61fe4fea4018b083b6
Jan Rohwer <jan.rohwer@gmail.com>
2025-03-26 08:07:43 +0100

    new task: clean (with script)

---
+++ scripts/clean.js
@@ -0,0 +1,23 @@
+const { execSync } = require("child_process");
+
+const CLEANUP_FOLDERS = [
+  "apps/webapp/.angular",
+  "apps/webapp/dist",
+  "apps/webapp/node_modules",
+  "packages/**/node_modules",
+  "packages/**/dist",
+  "node_modules",
+];
+
+function clean() {
+  console.log("🧹 Starting cleanup...");
+
+  CLEANUP_FOLDERS.forEach((folder) => {
+    console.log(`Cleaning ${folder}...`);
+    execSync(`rimraf -g "${folder}"`, { stdio: "inherit" });
+  });
+
+  console.log("✨ Cleanup completed!");
+}
+
+clean();
